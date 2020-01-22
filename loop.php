2636b0eb31620807c5299e5133e27ae2b32d4af1
sabeen12345 <59909957+sabeen12345@users.noreply.github.com>
2020-01-23 00:28:42 +0500

    loop

---
+++ loop.php
@@ -0,0 +1,31 @@
+<?php
+ 	echo "<h2>Hello</h2> <br>";
+ 	if (isset ($_POST['number'])){
+ 	$num = $_POST['number'];
+ 	
+
+ 	
+ 	for ($i=1; $i<=10; $i++){
+ 		echo $i * $num ;
+ 	}
+ }
+ 		
+ 	
+ 
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<form method ="POST">
+		<label>Number</label>
+		<input type="number" name="number">
+		<input type="submit" value="send">
+	</form>
+	</body>
+</html>
